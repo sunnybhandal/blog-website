3cf346b311f4c12e9131e05d9ef073c7e4e8b548
sunnybhandal <sunnybhandal123@gmail.com>
2023-06-04 19:07:08 -0600

    some navbar stuff

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import classes from "./navbar.module.css";
+import Link from "next/link";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <h2 className={classes.left}>
+          <Link href="/">Sunny Bhandal</Link>
+        </h2>
+        <ul className={classes.left}></ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
