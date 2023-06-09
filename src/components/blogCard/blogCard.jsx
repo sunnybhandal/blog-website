b055405c2dd10b70f5d8b047fc9d8fce456cf93c
sunnybhandal <sunnybhandal123@gmail.com>
2023-06-09 16:46:29 -0600

    blogcard

--- src/components/blogCard/blogCard.jsx
+++ src/components/blogCard/blogCard.jsx
@@ -3,10 +3,20 @@ import classes from "./blogCard.module.css";
 import Link from "next/link";
 
 const BlogCard = ({ blog: { title, desc, authorId } }) => {
+  const isLiked = true;
   return (
     <div className={classes.container}>
       <div className={classes.wrapper}>
-        <Link></Link>
+        <div className={classes.blogData}>
+          <h3>{title}</h3>
+          <p>{desc}</p>
+          <span>
+            Created By: <span>Jan 1st</span>
+          </span>
+        </div>
+        <div className={classes.right}>
+          {12} {isLiked ? <p>icon</p> : <p>close</p>}
+        </div>
       </div>
     </div>
   );
