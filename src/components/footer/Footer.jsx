cf2fd4a2cf6e532687a920c61e4e4e1dceafc094
sunnybhandal <sunnybhandal123@gmail.com>
2023-06-09 16:52:08 -0600

    before db stuff

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,25 @@ import React from "react";
 import classes from "./footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div>
+        <div className={classes.col}>
+          <h2>About the App</h2>
+          <p>Description stuff</p>
+        </div>
+        <div className={classes.col}>
+          <h2>Contacts</h2>
+          <p>403-123-1234</p>
+          <p>GitHub</p>
+        </div>
+        <div className={classes.col}>
+          <h2>Location</h2>
+          <p>Calgary</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
