1538c14125792903208f830b10789e0a15763e9a
sunnybhandal <sunnybhandal123@gmail.com>
2023-06-15 20:21:40 -0600

    big

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,1 +1,11 @@
-"http://localhost:3000/api/register";
+import db from "@/lib/db";
+import bcrypt from "bcrypt";
+import User from "@/models/User";
+
+export async function POST(req) {
+  try {
+    db.connect();
+  } catch (error) {
+    return Response(JSON.stringify(error.message), { status: 500 });
+  }
+}
