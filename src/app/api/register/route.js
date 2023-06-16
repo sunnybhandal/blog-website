1722598773bb8b77d8fcd2e1c92477cedf5d5ed3
sunnybhandal <sunnybhandal123@gmail.com>
2023-06-16 14:45:44 -0600

    register

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -4,8 +4,28 @@ import User from "@/models/User";
 
 export async function POST(req) {
   try {
-    db.connect();
+    await db.connect();
+
+    const { username, email, password: pass } = await req.json();
+
+    const isExisting = await User.findOne({ email });
+
+    if (isExisting) {
+      throw new Error("User already exists");
+    }
+
+    const hashedPassword = await bcrypt.hash(pass, 10);
+
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    const { password, ...user } = newUser._doc;
+
+    return new Response(JSON.stringify(user), { status: 201 });
   } catch (error) {
-    return Response(JSON.stringify(error.message), { status: 500 });
+    return new Response(JSON.stringify(error.message), { status: 500 });
   }
 }
