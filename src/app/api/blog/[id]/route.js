63f36baae0eeea4253584054ef749a5ced04f9c2
sunnybhandal <sunnybhandal123@gmail.com>
2023-06-17 18:14:31 -0600

    get, update blogs

---
+++ src/app/api/blog/[id]/route.js
@@ -0,0 +1,60 @@
+import db from "@/lib/db";
+import { verifyJwtToken } from "@/lib/jwt";
+import Blog from "@/models/Blog";
+import User from "@/models/User";
+
+export async function GET(req, ctx) {
+  await db.connect();
+
+  const id = ctx.params.id;
+
+  try {
+    const blog = await Blog.findById(id)
+      .populate("authorId")
+      .select("-password");
+
+    return new Response(JSON.stringify(blog), { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify(null), { status: 500 });
+  }
+}
+
+export async function PUT(req, ctx) {
+  await db.connect();
+  const id = ctx.params.id;
+  const accessToken = req.headers.get("authorization");
+
+  const token = accessToken.split(" ")[1];
+  const decodedToken = verifyJwtToken(token);
+
+  if (!accessToken || !decodedToken) {
+    return new Response(
+      JSON.stringify({ error: "unauthorized (wrong or expired token" }),
+      { status: 403 }
+    );
+  }
+
+  try {
+    const body = await req.json();
+    const blog = await Blog.findById(id).populate("authorId");
+
+    if (blog?.authorId?._id.toString() !== decodedToken._id.toString()) {
+      return new Response(
+        JSON.stringify({ msg: "Only the author can update his blog" }),
+        { status: 403 }
+      );
+    }
+
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      id,
+      { $set: { ...body } },
+      { new: true }
+    );
+
+    return new Response(JSON.stringify(updatedBlog), { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify(null), { status: 500 });
+  }
+}
+
+export async function DELETE(req) {}
